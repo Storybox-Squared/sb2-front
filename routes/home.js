b9d6f3a3cb409444631981e499abadd54eff419f
Tom Alabaster <tomalabasteruk@gmail.com>
2016-12-16 20:55:17 +0000

    Updated dependencies.

--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,6 @@ var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
 var router = express.Router();
 
 router.get('/', ensureLoggedIn, function (req, res) {
-    console.log(req);
     return res.render('home', {title: 'SB2 Assets '});
 });
 
