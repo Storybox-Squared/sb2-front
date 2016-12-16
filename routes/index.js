8d92cb6ca1b95ca3ef4585ba6f6b0d194acbce3c
Tom Alabaster <tomalabasteruk@gmail.com>
2016-12-16 20:48:34 +0000

    Included .env file.

--- routes/index.js
+++ routes/index.js
@@ -25,8 +25,8 @@ router.get('/logout', function (req, res) {
     res.redirect('/');
 });
 
-// Perform the final stage of authentication and redirect to '/user'
-router.get('/callback', passport.authenticate('auth0', {failureRedirect: '/url-if-something-fails'}), function (req, res) {
+// Perform the final stage of authentication and redirect
+router.get('/callback', passport.authenticate('auth0', {failureRedirect: '/login'}), function (req, res) {
     res.redirect(req.session.returnTo || '/home');
 });
 
